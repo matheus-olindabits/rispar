8f06d815d57a2bc656bd9f4d86b7e856cd33320b
Matheus Leal <matheus.leal@nutrin.com.br>
2022-06-25 00:39:09 -0300

    change dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:rispar_project/app/app_module.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 // ignore: depend_on_referenced_packages, library_prefixes
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:rispar_project/app/app_widget.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  await loadEnv();
+  await dotenv.load(fileName: '.env'); 
   runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+  FlutterNativeSplash.remove();
 }
 
-Future<void> loadEnv() async {
-  await dotenv.load(fileName: '.env'); 
-}
